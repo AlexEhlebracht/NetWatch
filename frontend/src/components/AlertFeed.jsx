ae3b7c9514621fe3de90e7f89c728b6046a7a75b
AlexEhlebracht <alex.ehlebracht@gmail.com>
2026-04-06 22:19:57 -0500

    devices

--- frontend/src/components/AlertFeed.jsx
+++ frontend/src/components/AlertFeed.jsx
@@ -3,7 +3,8 @@ import axios from "axios";
 
 export default function AlertFeed({ wsData }) {
   const [alerts, setAlerts] = useState([]);
-  const API_URL = import.meta.env.VITE_API_URL || "http://192.168.1.108:8000";
+  const API_URL =
+    import.meta.env.VITE_API_URL || `http://${window.location.hostname}`;
 
   useEffect(() => {
     axios
